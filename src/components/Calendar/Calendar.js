974ac1c7cbeeda62413b4eeb6dc33c69baa0a211
charlesmtobias <charlesmtobias@gmail.com>
2023-08-27 01:37:53 -0500

    fix bug where calendar days when past end of month (ie 30, 31, 32, 33 etc)

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -5,12 +5,14 @@ export default function Calendar() {
     const FIRST_DATE = getFirstDateThisWeek();
     const DAYS_OF_THE_WEEK = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
     let calendar = [];
-
+    let newDate = new Date();
     for (let i = 0; i < 7; i++) {
+        console.log(newDate.getDate());
         calendar.push(<div className='col px-0'>
                         <h6 className='fw-bold'>{DAYS_OF_THE_WEEK[i]}</h6>
-                        <button id={i} className={`btn btn-cal fw-semibold ${getCurrentDayOfWeek() == i ? 'active' : ''}`}>{FIRST_DATE + i}</button>
+                        <button id={i} className={`btn btn-cal fw-semibold ${getCurrentDayOfWeek() == i ? 'active' : ''}`}>{("0" + newDate.getDate()).slice(-2)}</button>
                     </div>);
+        newDate.setDate(newDate.getDate() + 1);
     }
     
     return(
@@ -25,7 +27,7 @@ function getFirstDateThisWeek() {
     var curr = new Date; // get current date
     var first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
 
-    var firstday = new Date(curr.setDate(first)).getDate();
+    var firstday = new Date(curr.setDate(first));
     return firstday;
 }
 
